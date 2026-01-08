ad7adaab75526f73e81d2baaae85551e15e649a1
Harikaish karthikeyan <harikaish17@gmail.com>
2026-01-08 14:33:46 +0530

    Add files via upload

---
+++ practice/shop.java
@@ -0,0 +1,10 @@
+public class shop {
+public static void main(String[] args) {
+int itema=200, itemb=300, itemc=400;
+double price;
+price=((itema*2)+(itemb)+(itemc*3));
+price=price-(price*0.1);
+price=price+(price*0.05);
+System.out.println(price);
+}
+}
