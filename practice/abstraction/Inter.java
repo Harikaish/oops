46ea8c0a7a5aec144af1d42847a91919f7adbbda
Harikaish karthikeyan <harikaish17@gmail.com>
2026-04-09 14:14:40 +0530

    Add files via upload

---
+++ practice/abstraction/Inter.java
@@ -0,0 +1,78 @@
+
+interface VoiceControlled {
+    void voiceCommand(String command);
+}
+
+class Appliance {
+    String name;
+
+    Appliance(String name) {
+        this.name = name;
+    }
+
+    void turnOn() {
+        System.out.println(name + " is turned ON");
+    }
+
+    void turnOff() {
+        System.out.println(name + " is turned OFF");
+    }
+}
+
+class SmartLight extends Appliance implements VoiceControlled {
+
+    SmartLight(String name) {
+        super(name);
+    }
+
+    @Override
+    public void voiceCommand(String command) {
+        System.out.println("SmartLight received voice command: " + command);
+    }
+
+    // Overriding turnOn()
+    @Override
+    void turnOn() {
+        System.out.println(name + " (SmartLight) is glowing");
+    }
+}
+
+class SmartThermostat extends Appliance implements VoiceControlled {
+
+    SmartThermostat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void voiceCommand(String command) {
+        System.out.println("SmartThermostat received voice command: " + command);
+    }
+
+    // Overriding turnOn()
+    @Override
+    void turnOn() {
+        System.out.println(name + " (Thermostat) is regulating temperature");
+    }
+}
+
+public class Inter {
+    public static void main(String[] args) {
+
+        // Runtime polymorphism
+        Appliance a1 = new SmartLight("Living Room Light");
+        Appliance a2 = new SmartThermostat("Room Thermostat");
+
+        a1.turnOn();
+        a1.turnOff();
+
+        a2.turnOn();
+        a2.turnOff();
+
+        // Interface reference
+        VoiceControlled v1 = new SmartLight("Bedroom Light");
+        VoiceControlled v2 = new SmartThermostat("Hall Thermostat");
+
+        v1.voiceCommand("Turn on light");
+        v2.voiceCommand("Set temperature to 24°C");
+    }
+}
